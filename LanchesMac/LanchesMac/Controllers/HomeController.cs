548cc0a7fca6327ba74a5235d31dcb499f169258
Luiz Henrique Caires <spcairesads@gmail.com>
2023-02-16 22:45:09 -0300

    Atualização 14

--- LanchesMac/LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/LanchesMac/Controllers/HomeController.cs
@@ -11,7 +11,11 @@ namespace LanchesMac.Controllers
         {
             return View();
         }
-        
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
